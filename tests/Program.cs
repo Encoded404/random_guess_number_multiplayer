2061e05c19e4cd5912a1fbd685f0a77d8e659346
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-02-25 14:14:43 +0100

    Update Program.cs

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,13 +1,14 @@
 ﻿string input = "10";
 
 void print(string s){
-    Console.SetCursorPosition(0, Console.CursorTop - 1);
+    Console.SetCursorPosition(0, Console.CursorTop - 2);
     Console.WriteLine(s);
     Console.SetCursorPosition(0, 0);
     Console.WriteLine(input);
 }
 
 Console.Clear();
+Console.WriteLine("");
 Console.WriteLine("");
 
 print("wf");
