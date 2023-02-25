bd445a8772af5b4eb773782b2a5c598e18e60819
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-02-25 13:29:52 +0100

    tets

---
+++ tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+
+
+void ThreadProc(){
+    Thread.Sleep(2000);
+    Console.WriteLine("rg");
+}
+Thread t = new Thread(new ThreadStart(ThreadProc));
+
+Console.ReadLine();
+
