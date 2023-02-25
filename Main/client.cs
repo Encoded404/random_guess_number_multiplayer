3fd2aaa8227cb615d12ca6a090574fefdd6d2229
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-02-25 14:57:13 +0100

    printTools

--- Main/client.cs
+++ Main/client.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Threading;
+
+Programy.print("r");
 Console.WriteLine("Hello, World!");
 
 Game game = new Game(new Range(1, 10));
