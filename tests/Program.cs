52db2b081e3a644f6047da77d69ff28cd08281e3
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-02-25 14:48:32 +0100

    Update Program.cs

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,17 +1,69 @@
-﻿string input = "10";
+﻿using System.Threading;
+string input = "10";
 
 void print(string s){
-    Console.SetCursorPosition(0, Console.CursorTop - 2);
-    Console.WriteLine(s);
-    Console.SetCursorPosition(0, 0);
-    Console.WriteLine(input);
+    Console.SetCursorPosition(0, Console.CursorTop - 0);
+    if(input.Length > s.Length) Console.WriteLine(s + Spaces(input.Length - s.Length));
+    else Console.WriteLine(s);
+    // Console.WriteLine(s);
+    // Console.WriteLine("");
+    Console.Write(input);
+    
 }
+string Spaces(int n)
+{
+    return new string(' ', n);
+}
+
+
+void MonitorKeypress()
+{
+  ConsoleKeyInfo cki = new ConsoleKeyInfo();
+  do
+  {
+    // true hides the pressed character from the console
+    cki = Console.ReadKey(true);
+    Console.Write(cki.KeyChar);
+    input += cki.KeyChar;
+    
+    // Wait for an ESC
+  } while (cki.Key != ConsoleKey.Escape);
+  
+  // Cancel the token
+}
+
+Thread t = new Thread(MonitorKeypress);
+
+t.Start();
+
 
 Console.Clear();
 Console.WriteLine("");
 Console.WriteLine("");
 
+
+
+// ... Launch other async tasks ...
+
 print("wf");
+Thread.Sleep(1000);
+print("hej");
+Thread.Sleep(1000);
+print("du");
+Thread.Sleep(1000);
+print("wf");
+Thread.Sleep(1000);
+print("hej");
+Thread.Sleep(1000);
+print("du");
+Thread.Sleep(1000);
+print("wf");
+Thread.Sleep(1000);
+print("hej");
+Thread.Sleep(1000);
+print("du");
+Thread.Sleep(1000);
+// Waits for the keypress to end it all
 
 // he is cool
 // lala
