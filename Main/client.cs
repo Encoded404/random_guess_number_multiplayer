3c0657215b55ef791340432ad1f8a807d4bdf028
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-02-25 13:10:49 +0100

    added client code

--- Main/client.cs
+++ Main/client.cs
@@ -1,1 +1,73 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Game game = new Game(new Range(1, 10));
+
+string wantsToPlay = "";
+while(!wantsToPlay.ToLower().StartsWith("n")){
+    game.Play();
+    wantsToPlay = input.i("Wanna play agian? ");
+    
+}
+static public class input
+{
+    public static string i(string question = ""){
+
+        Console.Write(question);
+        return Console.ReadLine();
+    }
+}
+
+
+class Game{
+    System.Random random = new System.Random();
+    private Range range;
+    private int CorrectNumber;
+
+    public Game(Range range){
+        this.range = range;
+    }
+
+    // string input(string question = "")
+    // {
+    //     Console.Write(question);
+    //     return Console.ReadLine();
+    // }
+
+
+    public int Play(){
+        CorrectNumber = random.Next(range.lowest, range.highest);
+        int chosenNumber = Int32.Parse(input.i("First guess: "));
+        int numGuesses = 0;
+        while (chosenNumber != CorrectNumber)
+        {
+            numGuesses++;
+            if(chosenNumber < CorrectNumber){
+                Console.WriteLine("Too low. Guess higher.");
+            }
+            if(chosenNumber > CorrectNumber){
+                Console.WriteLine("Too high. Guess lower.");
+            }
+            chosenNumber =  Int32.Parse(input.i("Guess: "));
+
+        }
+
+        Console.WriteLine("Congratulations! You guessed the number in {0} guesses.", numGuesses);
+
+        return numGuesses;
+    }
+}
+
+
+class Range{
+    public int lowest;
+    public int highest;
+
+    public Range(int lowest, int highest){
+        this.lowest = lowest;
+        this.highest = highest;
+    }
+}
+
+
+
